a2541944828f2c8c6c9337ef94965751f145f91b
Süleyman Akgün <suleyman17103@gmail.com>
2024-01-21 22:01:36 +0300

    US_09-TC_01

---
+++ src/test/java/otomasyontest/US_09.java
@@ -0,0 +1,74 @@
+package otomasyontest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class US_09 {
+    WebDriver driver;
+    @BeforeEach
+    public void setUp() {
+         driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://allovercommerce.com/");
+    }
+
+    @Test
+    public void tc_01() throws InterruptedException {
+        List<WebElement> register =driver.findElements(By.tagName("span"));
+
+
+        for (WebElement w:register) {
+
+            if (w.getText().equals("Register")){
+
+                w.click();
+            }
+
+        }
+
+        WebElement a =driver.findElement(By.linkText("Become a Vendor"));
+        a.click();
+
+        driver.findElement(By.id("user_email")).sendKeys("haeden.kervin@falkcia.com");
+        driver.findElement(By.id("passoword")).sendKeys("1234Sa.");
+        driver.findElement(By.id("confirm_pwd")).sendKeys("1234Sa.");
+        WebElement register1=driver.findElement(By.id("wcfm_membership_register_button"));
+
+        Thread.sleep(60000);
+        register1.click();
+
+        List<WebElement> tag=driver.findElements(By.tagName("h1"));
+
+
+        for (WebElement w:tag) {
+
+            if (w.getText().equals("Welcome to Allover Commerce!")){
+                System.out.println(w.getText());
+
+                Assertions.assertEquals("Welcome to Allover Commerce!",w.getText());
+
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+}
